88f32b6b1c8a0d8ada83f25f778ca2da44d4461a
917loollo <917loollo@gmail.com>
2026-09-25 15:34:38 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return <main className="shell"><section className="glass hero"><p className="label">DOYUN HOST</p><h1 className="title">Your site.<br/>Your URL.</h1><p className="sub">HTML 사이트를 만들고 <b>/원하는이름</b>으로 공개하세요.</p><div className="spacer"/><div className="row"><a className="btn" href="/host">관리자 열기</a></div></section></main>;
-}
